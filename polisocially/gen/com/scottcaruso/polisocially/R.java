48f52108025c705d6ddf3915e718be67513c7d7e
Scott Caruso <scottcaruso@fullsail.edu>
2013-11-07 09:54:48 -0500

    Framework for Main_Screen in place.

--- polisocially/gen/com/scottcaruso/polisocially/R.java
+++ polisocially/gen/com/scottcaruso/polisocially/R.java
@@ -24,7 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int imageButton1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main__screen=0x7f030000;
@@ -38,6 +39,8 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int main_geolocation_button=0x7f050003;
+        public static final int zip_code_entry=0x7f050004;
     }
     public static final class style {
         /** 
