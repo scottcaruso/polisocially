616daadf13f80bdd1de494fd83c8297ee37e4a65
Scott Caruso <scottcaruso@fullsail.edu>
2013-11-07 14:40:48 -0500

    Button created and implemented.

--- polisocially/gen/com/scottcaruso/polisocially/R.java
+++ polisocially/gen/com/scottcaruso/polisocially/R.java
@@ -22,10 +22,13 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int main_button=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080003;
+        public static final int button1=0x7f080001;
         public static final int imageButton1=0x7f080000;
+        public static final int textView1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main__screen=0x7f030000;
@@ -37,6 +40,7 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int all_politiican_data_courtesy_of_sunlight_labs_=0x7f050005;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
         public static final int main_geolocation_button=0x7f050003;
