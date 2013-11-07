63c76a936ba5b3aafa8f2f9552b07e51ebda8e36
Scott Caruso <scottcaruso@fullsail.edu>
2013-11-07 16:47:47 -0500

    Layout for politiican results screen.

--- polisocially/gen/com/scottcaruso/polisocially/R.java
+++ polisocially/gen/com/scottcaruso/polisocially/R.java
@@ -23,12 +23,15 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int main_button=0x7f020001;
+        public static final int map=0x7f020002;
     }
     public static final class id {
-        public static final int About=0x7f080003;
-        public static final int action_settings=0x7f080004;
+        public static final int About=0x7f080005;
+        public static final int action_settings=0x7f080006;
         public static final int button1=0x7f080001;
         public static final int imageButton1=0x7f080000;
+        public static final int imageView1=0x7f080003;
+        public static final int listView1=0x7f080004;
         public static final int textView1=0x7f080002;
     }
     public static final class layout {
@@ -47,6 +50,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
         public static final int main_geolocation_button=0x7f050003;
+        public static final int placeholder_map=0x7f050008;
         public static final int zip_code_entry=0x7f050004;
     }
     public static final class style {
