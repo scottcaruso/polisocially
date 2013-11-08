382dcd91e7ec991c6a72c4e9983e118e924f3b74
Scott Caruso <scottcaruso@fullsail.edu>
2013-11-08 04:03:44 -0500

    Layout of politician details page done.

--- polisocially/gen/com/scottcaruso/polisocially/R.java
+++ polisocially/gen/com/scottcaruso/polisocially/R.java
@@ -27,12 +27,19 @@ public final class R {
         public static final int no_image_available=0x7f020003;
     }
     public static final class id {
-        public static final int About=0x7f080005;
-        public static final int action_settings=0x7f080006;
+        public static final int About=0x7f08000c;
+        public static final int action_settings=0x7f08000d;
         public static final int button1=0x7f080001;
+        public static final int buttonFollow=0x7f080008;
+        public static final int buttonPhone=0x7f08000a;
+        public static final int buttonTweet=0x7f080009;
         public static final int imageButton1=0x7f080000;
-        public static final int imageView1=0x7f080003;
-        public static final int listView1=0x7f080004;
+        public static final int imageView1=0x7f080004;
+        public static final int listView1=0x7f08000b;
+        public static final int poliAge=0x7f080006;
+        public static final int poliHometown=0x7f080007;
+        public static final int poliName=0x7f080003;
+        public static final int poliParty=0x7f080005;
         public static final int textView1=0x7f080002;
     }
     public static final class layout {
@@ -47,11 +54,17 @@ public final class R {
         public static final int about=0x7f050007;
         public static final int about_app=0x7f050006;
         public static final int action_settings=0x7f050001;
+        public static final int age=0x7f05000b;
         public static final int all_politiican_data_courtesy_of_sunlight_labs_=0x7f050005;
         public static final int app_name=0x7f050000;
+        public static final int call=0x7f05000e;
+        public static final int follow=0x7f05000d;
         public static final int hello_world=0x7f050002;
+        public static final int hometown=0x7f05000c;
         public static final int main_geolocation_button=0x7f050003;
+        public static final int party=0x7f05000a;
         public static final int placeholder_map=0x7f050008;
+        public static final int politiican_name=0x7f050009;
         public static final int zip_code_entry=0x7f050004;
     }
     public static final class style {
